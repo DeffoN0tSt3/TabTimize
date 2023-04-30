f44d5f16b5a2bb5228dc011eb9caef9cc3fdd4db
n0tst3 <s@realinfosec.net>
2023-04-30 13:11:46 +0100

    Update popup.js
    refactored code, incorporated the use of async/await, improved variable naming, error handling, and code readability

--- popup.js
+++ popup.js
@@ -1,70 +1,89 @@
 // A lightweight google chrome tab optimizer by @n0tst3
-function stackTabs() {
-    var tabsByDomain = {};
-    chrome.tabs.query({ currentWindow: true }, function (tabs) {
-        for (var i = 0; i < tabs.length; i++) {
-            var tab = tabs[i];
-            var url = new URL(tab.url);
-            var domain = url.hostname;
-            if (domain in tabsByDomain) {
+async function stackTabs() {
+    const tabs = await chrome.tabs.query({ currentWindow: true });
+    const tabsByDomain = groupTabsByDomain(tabs);
+    await createTabGroups(tabsByDomain);
+}
+
+function groupTabsByDomain(tabs) {
+    let tabsByDomain = {};
+
+    tabs.forEach((tab) => {
+        if (isValidURL(tab.url)) {
+            const url = new URL(tab.url);
+            const domain = url.hostname;
+
+            if (tabsByDomain[domain]) {
                 tabsByDomain[domain].push(tab.id);
             } else {
                 tabsByDomain[domain] = [tab.id];
             }
         }
-        for (var domain in tabsByDomain) {
-            var tabIds = tabsByDomain[domain];
+    });
 
-            chrome.tabs.group({ tabIds: tabIds }, function (groupId) {
-                var tabsInGroup = tabIds.length;
-                var tabIndices = tabsInGroup > 1 ? [0] : [0, 1];
-                chrome.tabs.highlight({ tabs: tabIndices });
-            });
+    return tabsByDomain;
+}
+
+async function createTabGroups(tabsByDomain) {
+    for (const domain in tabsByDomain) {
+        const tabIds = tabsByDomain[domain];
+
+        const groupId = await chrome.tabs.group({ tabIds: tabIds });
+        const tabsInGroup = tabIds.length;
+        const tabIndices = tabsInGroup > 1 ? [0] : [0, 1];
+
+        await chrome.tabs.highlight({ tabs: tabIndices });
+    }
+}
+
+async function onTabCreate(tab) {
+    if (isValidURL(tab.url)) {
+        const existingTab = await findTabWithMatchingDomain(tab);
+        if (existingTab) {
+            await chrome.tabs.group({ groupId: existingTab.groupId, tabIds: [tab.id] });
+            console.log('Tab grouped successfully');
         }
+    }
+}
+
+async function onTabUpdate(tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete' && isValidURL(tab.url)) {
+        const existingTab = await findTabWithMatchingDomain(tab);
+        if (existingTab) {
+            await chrome.tabs.group({ groupId: existingTab.groupId, tabIds: [tabId] });
+            console.log('Tab grouped successfully');
+        }
+    }
+}
+
+async function findTabWithMatchingDomain(tab) {
+    const tabs = await chrome.tabs.query({ currentWindow: true });
+    const domain = new URL(tab.url).hostname;
+    return tabs.find((t) => {
+        return new URL(t.url).hostname === domain && t.id !== tab.id;
     });
 }
-function onTabCreate(tab) {
-    if (tab.url) {
-        chrome.tabs.query({ currentWindow: true }, function (tabs) {
-            var existingTab = tabs.find(function (t) {
-                var url = new URL(t.url);
-                var domain = url.hostname;
-                return domain === new URL(tab.url).hostname && t.id !== tab.id;
-            });
-            if (existingTab) {
-                chrome.tabs.group({ groupId: existingTab.groupId, tabIds: [tab.id] }, function () {
-                    console.log('Tab grouped successfully');
-                });
-            }
-        });
+
+function isValidURL(url) {
+    try {
+        new URL(url);
+        return true;
+    } catch (err) {
+        return false;
     }
 }
-function onTabUpdate(tabId, changeInfo, tab) {
-    if (changeInfo.status === 'complete') {
-        chrome.tabs.query({ currentWindow: true }, function (tabs) {
-            var existingTab = tabs.find(function (t) {
-                var url = new URL(t.url);
-                var domain = url.hostname;
-                return domain === new URL(tab.url).hostname && t.id !== tab.id;
-            });
-            if (existingTab) {
-                chrome.tabs.group({ groupId: existingTab.groupId, tabIds: [tabId] }, function () {
-                    console.log('Tab grouped successfully');
-                });
-            }
-        });
-    }
-}
+
 function autoSortTabsByDomain(enabled) {
     if (enabled) {
         chrome.tabs.onCreated.addListener(onTabCreate);
         chrome.tabs.onUpdated.addListener(onTabUpdate);
-        stackTabs(); 
+        stackTabs();
     } else {
         chrome.tabs.onCreated.removeListener(onTabCreate);
         chrome.tabs.onUpdated.removeListener(onTabUpdate);
     }
 }
+
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('stack-tabs-btn').addEventListener('click', stackTabs);
 
@@ -74,7 +93,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.getElementById('auto-sort-checkbox').addEventListener('change', function () {
-        var enabled = this.checked;
+        const enabled = this.checked;
         chrome.storage.sync.set({ 'autoSortEnabled': enabled }, function () {
             autoSortTabsByDomain(enabled);
         });
